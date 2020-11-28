413a66f0b4c96577c4a85e4d9d1a642ec9de90d4
nas- <nasil2nd@gmail.com>
2020-11-28 09:58:46 +0100

    small updates

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-CONFIG = {'LINKED': 'https://www.marinetraffic.com/en/ais/home/centerx:-14.9/centery:-39.6/zoom:11',
-          'BOSS': 'https://www.marinetraffic.com/en/ais/home/centerx:-20.928/centery:-37.930/zoom:13',
-          'APIVIA': 'https://www.marinetraffic.com/en/ais/home/centerx:-8.4/centery:-42.1/zoom:9',
-          'CAM': 'https://www.marinetraffic.com/en/ais/home/centerx:-14.1/centery:-36.6/zoom:11',
-           'ARKEA': 'https://www.marinetraffic.com/en/ais/home/centerx:-18.304/centery:-38.045/zoom:14',
-           'INITIATIVE': 'https://www.marinetraffic.com/en/ais/home/centerx:-20.849/centery:-39.278/zoom:12',
-           'BURTON': 'https://www.marinetraffic.com/en/ais/home/centerx:-19.6/centery:-40.0/zoom:11'
+CONFIG = {'LINKED': 'https://www.marinetraffic.com/en/ais/home/centerx:-10.3/centery:-40.5/zoom:10',
+          'BOSS': 'https://www.marinetraffic.com/en/ais/home/centerx:-17.578/centery:-37.944/zoom:17',
+          'APIVIA': 'https://www.marinetraffic.com/en/ais/home/centerx:-3.3/centery:-41.7/zoom:11',
+          'CAM': 'https://www.marinetraffic.com/en/ais/home/centerx:-9.415/centery:-37.180/zoom:14',
+           'ARKEA': 'https://www.marinetraffic.com/en/ais/home/centerx:-12.641/centery:-38.763/zoom:18',
+           'INITIATIVE': 'https://www.marinetraffic.com/en/ais/home/centerx:-15.860/centery:-40.419/zoom:17',
+           'BURTON': 'https://www.marinetraffic.com/en/ais/home/centerx:-13.829/centery:-41.055/zoom:12'
           }
